03a00260ea0f0f78722b4252e6691d53aa8901ee
Blake Price <blakeandrewprice@gmail.com>
2019-10-21 20:30:03 -0700

    Added labels, pickers, buttons and text fields to storyboard. Created countdown/controller swift files.

--- Dodo/Dodo/CountdownController.swift
+++ Dodo/Dodo/CountdownController.swift
@@ -1,8 +1,8 @@
 //
-//  Dodo.swift
-//  Dodo (like the bird)
+//  CountdownController.swift
+//  Dodo
 //
-//  Created by Blake Andrew Price on 10/17/19.
+//  Created by Blake Andrew Price on 10/21/19.
 //  Copyright © 2019 Blake Andrew Price. All rights reserved.
 //
 

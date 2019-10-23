01d27c4c32b886e794dea9e9df1bea9393c56814
Blake Price <blakeandrewprice@gmail.com>
2019-10-22 20:01:30 -0700

    Added: CountdownDetailViewController, delete editing style. Few typos and connection issue fixed.

---
+++ Dodo/Dodo/CountdownDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CountdownDetailViewController.swift
+//  Dodo
+//
+//  Created by Blake Andrew Price on 10/22/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class CountdownDetailViewController: UIViewController {
+    //MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+        
+        let tap = UITapGestureRecognizer(target: self.view,
+                                         action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    //MARK: - Outlets
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    //MARK: - Properties
+    var countdownController: CountdownController?
+    var countdown: Countdown?
+    
+    //MARK: - Actions
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        if let countdown = countdown {
+            countdownController?.updateTitleOrDate(title: titleTextField.text ?? "",
+                                                   dateAndTime: datePicker.date,
+                                                   for: countdown)
+        } else {
+            countdownController?.createCountdown(title: titleTextField.text ?? "",
+                                                 dateAndTime: datePicker.date)
+        }
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    
+    //MARK: - Functions
+    func updateViews() {
+        if let countdown = countdown {
+            titleTextField.text = countdown.title
+            textView.text = countdown.readableInterval
+            datePicker.date = countdown.dateAndTime
+            navigationItem.title = countdown.title
+        } else {
+            navigationItem.title = "Add a New Countdown"
+        }
+    }
+}
--- Dodo/Dodo/CountdownTableViewController.swift
+++ Dodo/Dodo/CountdownTableViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class CountdownTableTableViewController: UITableViewController {
+class CountdownTableViewController: UITableViewController {
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        countdownController.loadFromPersistentStore()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,7 +29,6 @@ class CountdownTableTableViewController: UITableViewController {
         } else {
             return 2
         }
-        
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -49,10 +49,40 @@ class CountdownTableTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CountdownCell", for: indexPath) as? CountdownTableViewCell else { fatalError("A countdown cell was not found!") }
-        //let countdown = countdownFor(indexPath: indexPath)
-        //cell.countdown = countdown
+        let countdown = countdownFor(indexPath: indexPath)
+        cell.countdown = countdown
         cell.updateViews()
     
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            let countdown = countdownFor(indexPath: indexPath)
+            countdownController.deleteCountdown(countdown: countdown)
+            tableView.reloadData()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddCountdownSegue" {
+            if let countdownDetailVC = segue.destination as? CountdownDetailViewController {
+                countdownDetailVC.countdownController = countdownController
+            }
+        } else if segue.identifier == "ShowCountdownSegue" {
+            if let countdownDetailVC = segue.destination as? CountdownDetailViewController,
+                let selectedCell = sender as? CountdownTableViewCell {
+                countdownDetailVC.countdownController = countdownController
+                countdownDetailVC.countdown = selectedCell.countdown
+            }
+        }
+    }
+    
+    private func countdownFor(indexPath: IndexPath) -> Countdown {
+        if indexPath.section == 0 {
+            return countdownController.unfinishedCountdowns[indexPath.row]
+        } else {
+            return countdownController.finishedCountdowns[indexPath.row]
+        }
     }
 }

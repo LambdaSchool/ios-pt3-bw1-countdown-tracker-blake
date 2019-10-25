baff4770fe36004d4c916335a7311c24aa2e2e88
Blake Price <blakeandrewprice@gmail.com>
2019-10-24 19:11:53 -0700

    Minor tweaks

--- Dodo/Dodo/CountdownTableViewController.swift
+++ Dodo/Dodo/CountdownTableViewController.swift
@@ -50,9 +50,9 @@ class CountdownTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 0 {
-            return "Active Countdowns"
+            return "Active:"
         } else {
-            return "Finished Countdowns"
+            return "Complete:"
         }
     }
 
